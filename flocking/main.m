3424184102eda2a9bd03aba3955c22a6783d85a6
Gabriel <gabi_nd@yahoo.com>
2015-04-17 00:11:30 +0300

    Initial commit.

---
+++ flocking/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  flocking
+//
+//  Created by Gabi on 16/04/15.
+//  Copyright (c) 2015 gn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
