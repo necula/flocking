0f2ea050b9416dd555b40223c0f21eaea05f34f9
Gabriel <gabi_nd@yahoo.com>
2015-04-18 14:11:12 +0300

    Added Sprite class.

---
+++ flocking/Sprite.h
@@ -0,0 +1,18 @@
+#import <Foundation/Foundation.h>
+#import <GLKit/GLKit.h>
+
+@interface Sprite : NSObject
+
+@property (nonatomic) CGPoint       position;
+@property (nonatomic) CGSize        size;
+@property (nonatomic) GLKMatrix4    wvMatrix;
+
+-(instancetype)init;
+-(instancetype)initWith:(CGPoint)position size:(CGSize)size;
+
+-(void)update:(float)dt;
+-(void)updateMatrix;
+
+-(void)translateRelative:(CGPoint)point;
+
+@end
---
+++ flocking/Sprite.m
@@ -0,0 +1,40 @@
+#import "Sprite.h"
+
+@implementation Sprite
+
+-(instancetype)initWith:(CGPoint)position size:(CGSize)size
+{
+    self = [super init];
+    if(!self)
+        return nil;
+    
+    _position = position;
+    _size = size;
+    _wvMatrix = GLKMatrix4Identity;
+    
+    return self;
+}
+
+-(instancetype)init
+{
+    return [self initWith:CGPointMake(0.f, 0.f) size:CGSizeMake(100.f, 100.f)];
+}
+
+-(void)update:(float)dt
+{
+    [self updateMatrix];
+}
+
+-(void)updateMatrix
+{
+    _wvMatrix = GLKMatrix4MakeTranslation(_position.x, _position.y, 0.f);
+    _wvMatrix = GLKMatrix4Scale(_wvMatrix, _size.width, _size.height, 1.f);
+}
+
+-(void)translateRelative:(CGPoint)point
+{
+    _position.x += point.x;
+    _position.y += point.y;
+}
+
+@end
